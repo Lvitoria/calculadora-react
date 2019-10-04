ffcfcf84969ed06c8ec8caa4af2152df2bbb63a2
Lucas Vitoria Alves <lucasvitoriaalves@hotmail.com>
2019-10-04 16:07:38 -0300

    primeiro

---
+++ src/main/calculadora.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Calculadora extends Component{
+    render(){
+        return(
+            <div className="calculadora">
+                oi
+            </div>
+        )
+    }
+}
