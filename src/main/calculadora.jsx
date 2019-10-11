544f2073f2b06697d155cd611f1e7bd5d77b263c
Lucas Vitoria Alves <lucasvitoriaalves@hotmail.com>
2019-10-10 22:09:25 -0300

    3

--- src/main/calculadora.jsx
+++ src/main/calculadora.jsx
@@ -3,7 +3,18 @@ import '../main/Calculadora.css';
 import Button from '../components/Button';
 import Display  from '../components/Display';
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 export default class Calculadora extends Component{
+    
+    //criou um clone desse objeto e passou para o state
+    state = { ...initialState }
     
     //rever essa aula
     constructor(props){
@@ -14,7 +25,8 @@ export default class Calculadora extends Component{
     }
     
     clearMemory(){
-        console.log('limpar');
+        console.log("limpa");
+        this.setState({ ...initialState })
     }
     
     setOperation(operation){
@@ -22,14 +34,41 @@ export default class Calculadora extends Component{
     }
 
     addDigit(n){
-        console.log(n);
+
+        if(n === '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        //o clearDisplay é uma flag
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    
+        // o clearDisplay se for true  portanto ele vai pegar o valor que esta no json 
+        // initialState = { displayValue
+    
+        const currentValue  = clearDisplay ? '' : this.state.displayValue
+       
+        console.log(currentValue);
+
+        const valor  =  currentValue + n
+        // ou posso usar o mesmo nome displayValue
+        //ai fica assim
+        // this.setState({ displayValue, clearDisplay: false})
+        this.setState({ displayValue: valor, clearDisplay: false})
+
+        if(n !== "."){
+            const i = this.state.current
+            const nemValue = parseFloat(valor)
+            const values = [ ...this.state.values]
+            values[i] = nemValue
+            this.setState({ values})
+        }
     }
 
     render(){
     
         return(
             <div className="calculadora">
-                <Display value={100} />
+                <Display value={this.state.displayValue } />
                 <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/" click={this.setOperation} operation />
                 <Button label="7"  click={this.addDigit}/>
